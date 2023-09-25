9fd54a014e3cb7af8a3b5fdbac25f2e72414fbd3
vmasdani <valianmasdani@gmail.com>
2023-09-25 22:08:27 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+var millisDelay = 10000;
+var url = "https://api.live-spreadsheet.atras.my.id/users";
+
+async Task runRequest()
+{
+    while (true)
+    {
+        Console.WriteLine($"Helloworld {DateTime.Now}");
+
+        try
+        {
+            var resp = await new HttpClient().PostAsync(
+                url,
+                new StringContent("{}", Encoding.UTF8, "application/json")
+            );
+            Console.WriteLine(await resp.Content.ReadAsStringAsync());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        await Task.Delay(millisDelay);
+    }
+}
+
+await Task.Run(runRequest);
